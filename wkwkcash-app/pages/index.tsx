4880a5dd3c1a177ff641bf6e2f707304ceea7b3a
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-03-25 14:00:28 +0100

    🦖 Update index.tsx
    - Update homepage to render based on connection status

--- wkwkcash-app/pages/index.tsx
+++ wkwkcash-app/pages/index.tsx
@@ -1,23 +1,20 @@
 import {
   SmartWallet,
   useAddress,
-  useDisconnect,
+  useConnectionStatus,
   useWallet,
 } from "@thirdweb-dev/react";
 import { NextPage } from "next";
 
+import { Loader2 } from "lucide-react";
 import Connected from "../components/connected";
 import Login from "../components/login";
-import { Button } from "../components/ui/button";
 
 const Home: NextPage = () => {
+  const status = useConnectionStatus();
+
   const address = useAddress();
   const wallet = useWallet();
-
-  console.log(address);
-  console.log(wallet);
-
-  const disconnect = useDisconnect();
 
   return (
     <main className="flex items-center justify-center h-screen">
@@ -26,16 +23,17 @@ const Home: NextPage = () => {
           wallet instanceof SmartWallet ? (
             <Connected />
           ) : (
-            <div>
-              <p>Connecting...</p>
-              <Button onClick={async () => await disconnect()}>
-                Disconnect
-              </Button>
-            </div>
+            <p>Connecting...</p>
           )
-        ) : (
-          <Login />
-        )}
+        ) : null}
+
+        {status === "unknown" || status === "connecting" ? (
+          <div className="space-x-2 flex items-center">
+            <Loader2 className="w-4 h-4 animate-spin" />
+            <p>Connecting wallet...</p>
+          </div>
+        ) : null}
+        {status === "disconnected" ? <Login /> : null}
       </div>
     </main>
   );
