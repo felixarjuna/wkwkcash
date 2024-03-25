d46caf35080a6663e742a7f9cbea8731dccc4473
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-03-25 13:25:35 +0100

    🦧 Update index component

--- wkwkcash-app/pages/index.tsx
+++ wkwkcash-app/pages/index.tsx
@@ -1,10 +1,42 @@
+import {
+  SmartWallet,
+  useAddress,
+  useDisconnect,
+  useWallet,
+} from "@thirdweb-dev/react";
 import { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+
+import Connected from "../components/connected";
+import Login from "../components/login";
+import { Button } from "../components/ui/button";
 
 const Home: NextPage = () => {
+  const address = useAddress();
+  const wallet = useWallet();
+
+  console.log(address);
+  console.log(wallet);
+
+  const disconnect = useDisconnect();
+
   return (
-    <main className={styles.main}>
-      <div className="font-">hello world</div>
+    <main className="flex items-center justify-center h-screen">
+      <div className="w-full max-w-7xl p-12 flex flex-col justify-center items-center h-full">
+        {address ? (
+          wallet instanceof SmartWallet ? (
+            <Connected />
+          ) : (
+            <div>
+              <p>Connecting...</p>
+              <Button onClick={async () => await disconnect()}>
+                Disconnect
+              </Button>
+            </div>
+          )
+        ) : (
+          <Login />
+        )}
+      </div>
     </main>
   );
 };
